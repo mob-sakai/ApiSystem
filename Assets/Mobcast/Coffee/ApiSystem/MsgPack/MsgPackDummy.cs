630e6693cafe2cd564dfb3992e373332e8029cd0
Takashi Sakai <sakai@mobcast.jp>
2017-12-18 15:15:32 +0900

    Dummy MsgPack

---
+++ Assets/Mobcast/Coffee/ApiSystem/MsgPack/MsgPackDummy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MsgPack.Serialization
+{
+	// Dummy MessagePackMemberAttribute.
+	[AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+	public class MessagePackMemberAttribute : Attribute
+	{
+		public MessagePackMemberAttribute(int _) { }
+	}
+	
+	public class MessagePackSerializer
+	{
+		public static MessagePackSerializer Get(Type t) { return null; }
+		public byte[] PackSingleObject(object o) { return null; }
+		public object UnpackSingleObject(byte[] b) { return null; }
+		public static object UnpackMessagePackObject(byte[] b) { return null; }
+	}
+}
